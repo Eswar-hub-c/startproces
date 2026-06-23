c09670ef53ac8ca87e521b8bf16227e934ccf7d8
Eswar-hub-c <ewarchandrashekar5@gmail.com>
2026-06-23 21:17:47 +0530

    dd

--- components/FloatingActions.tsx
+++ components/FloatingActions.tsx
@@ -2,7 +2,7 @@ export default function FloatingActions() {
   return (
     <div className="floating-actions">
       <a
-        href="https://+916281289228"
+        href="https://+91 6281289228"
         target="_blank"
         rel="noopener noreferrer"
         className="whatsapp-btn"
