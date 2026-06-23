f738841e25b16f5de8bee3e41984c73b8f1c1021
Eswar-hub-c <ewarchandrashekar5@gmail.com>
2026-06-23 21:36:29 +0530

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"/>
+
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
